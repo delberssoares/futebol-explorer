60165914dd78b6b1e1de251c0b283ecc1216f0af
delberss <delberss@hotmail.com>
2025-05-11 01:37:06 -0300

    feature: adiciona screenshot dos artilheiros dos clubes

--- src/app/teamArtilheiros.tsx
+++ src/app/teamArtilheiros.tsx
@@ -1,8 +1,10 @@
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import * as MediaLibrary from 'expo-media-library';
 import { useLocalSearchParams } from 'expo-router';
-import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { ActivityIndicator, Alert, Dimensions, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Toast from 'react-native-toast-message';
+import ViewShot from 'react-native-view-shot';
 
 type Artilheiro = {
   nome: string;
@@ -14,6 +16,7 @@ type Team = {
   shield: string;
   artilheiros: Artilheiro[];
   updateArtilheiros: string;
+  cores?: { main: string; secondary: string }[];
 };
 
 const TeamArtilheiros: React.FC = () => {
@@ -21,6 +24,16 @@ const TeamArtilheiros: React.FC = () => {
   const [team, setTeam] = useState<Team | null>(null);
   const params = useLocalSearchParams();
   const teamString = params.team;
+
+  const viewShotRef = useRef<ViewShot>(null);
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState(false);
+  const screenHeight = Dimensions.get('window').height;
+
+  const [scrollEnabled, setScrollEnabled] = useState(false);
+
+  const handleContentSizeChange = (contentWidth: number, contentHeight: number) => {
+    setScrollEnabled(contentHeight > screenHeight);
+  };
 
   useEffect(() => {
     if (teamString) {
@@ -32,6 +45,13 @@ const TeamArtilheiros: React.FC = () => {
     }
   }, [teamString]);
 
+  useEffect(() => {
+    (async () => {
+      const { status } = await MediaLibrary.requestPermissionsAsync();
+      setHasMediaLibraryPermission(status === 'granted');
+    })();
+  }, []);
+
   const showToast = (message: string) => {
     Toast.show({
       type: 'info',
@@ -39,6 +59,30 @@ const TeamArtilheiros: React.FC = () => {
       position: 'bottom',
       visibilityTime: 2000,
     });
+  };
+
+  const handleCaptureAndSave = async () => {
+    if (!hasMediaLibraryPermission) {
+      Alert.alert('Permissão necessária', 'É necessário permitir o acesso à galeria para salvar a imagem.');
+      return;
+    }
+
+    try {
+      const viewShot = viewShotRef.current;
+
+      if (viewShot && typeof viewShot.capture === 'function') {
+        const uri = await viewShot.capture();
+        if (uri) {
+          await MediaLibrary.saveToLibraryAsync(uri);
+          Alert.alert('Sucesso', 'Imagem salva na galeria com sucesso!');
+        }
+      } else {
+        Alert.alert('Erro', 'Referência inválida para o componente de captura.');
+      }
+    } catch (error) {
+      console.error('Erro ao capturar e salvar a tela:', error);
+      Alert.alert('Erro', 'Não foi possível salvar a imagem.');
+    }
   };
 
   if (loading) {
@@ -50,48 +94,75 @@ const TeamArtilheiros: React.FC = () => {
   }
 
   const artilheiros = team.artilheiros;
+  const teamColors = team?.cores?.[0] || { main: '#FFFFFF', secondary: '#000000' };
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Image source={{ uri: team.shield }} style={styles.shield} />
-      <Text style={styles.updateText}>Última atualização no dia {team.updateArtilheiros}</Text>
-      <View style={styles.titlesHeader}>
-        <TouchableOpacity onPress={() => showToast('Posição')}>
-          <MaterialCommunityIcons name="trophy" size={24} style={styles.icon} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => showToast('Nome do jogador')}>
-          <MaterialCommunityIcons name="account" size={24} style={styles.icon} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => showToast('Quantidade de gols')}>
-          <MaterialCommunityIcons name="soccer" size={24} style={styles.icon} />
-        </TouchableOpacity>
-      </View>
-      {artilheiros?.length > 0 ? (
-        artilheiros.map((item, index) => (
-          <View key={index.toString()} style={styles.titleItem}>
-            <Text>{index + 1}</Text>
-            <Text style={styles.titleCompetition}>{item.nome}</Text>
-            <Text style={styles.titleYear}>{`${item.gols}`}</Text>
+    <View style={{ flex: 1 }}>
+      <TouchableOpacity style={[styles.captureIcon, { backgroundColor: teamColors.secondary }]} onPress={handleCaptureAndSave}>
+        <MaterialIcons name="camera-alt" size={30} color={teamColors.main} />
+      </TouchableOpacity>
+
+      <ScrollView
+        scrollEnabled={scrollEnabled}
+        onContentSizeChange={handleContentSizeChange}
+        contentContainerStyle={{ flexGrow: 1 }}
+      >
+
+        <ViewShot
+          ref={viewShotRef}
+          options={{ format: 'png', quality: 1 }}
+          style={styles.viewShot}
+        >
+
+          <View style={[styles.container, { backgroundColor: teamColors.main, minHeight: screenHeight }]}>
+            <Image source={{ uri: team?.shield }} style={styles.shield} />
+            <Text style={styles.updateText}>Última atualização no dia {team.updateArtilheiros}</Text>
+            <View style={styles.titlesHeader}>
+              <TouchableOpacity onPress={() => showToast('Posição')}>
+                <MaterialCommunityIcons name="trophy" size={24} style={styles.icon} />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => showToast('Nome do jogador')}>
+                <MaterialCommunityIcons name="account" size={24} style={styles.icon} />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => showToast('Quantidade de gols')}>
+                <MaterialCommunityIcons name="soccer" size={24} style={styles.icon} />
+              </TouchableOpacity>
+            </View>
+            {artilheiros?.length > 0 ? (
+              artilheiros.map((item, index) => (
+                <View key={index.toString()} style={styles.titleItem}>
+                  <Text>{index + 1}</Text>
+                  <Text style={styles.titleCompetition}>{item.nome}</Text>
+                  <Text style={styles.titleYear}>{`${item.gols}`}</Text>
+                </View>
+              ))
+            ) : (
+              <Text style={styles.noDataText}>Em breve</Text>
+            )}
           </View>
-        ))
-      ) : (
-        <Text style={styles.noDataText}>Em breve</Text>
-      )}
-      <Toast />
-    </ScrollView>
+        </ViewShot>
+        <Toast />
+      </ScrollView>
+    </View>
+
   );
 };
 
 const styles = StyleSheet.create({
+  viewShot: {
+    backgroundColor: '#ffffff',
+  },
   container: {
     alignItems: 'center',
-    padding: 16,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
   },
   shield: {
     width: 100,
     height: 100,
     marginVertical: 16,
     resizeMode: 'contain',
+    alignSelf: 'center', // Centraliza horizontalmente
   },
   titlesHeader: {
     width: '100%',
@@ -130,6 +201,8 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: '#666',
     marginBottom: 8,
+    alignSelf: 'center',
+
   },
   loadingIndicator: {
     flex: 1,
@@ -147,6 +220,14 @@ const styles = StyleSheet.create({
     color: 'gray',
     textAlign: 'center',
     marginTop: 20,
+  },
+  captureIcon: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    padding: 10,
+    borderRadius: 30,
+    zIndex: 1,
   },
 });
 
